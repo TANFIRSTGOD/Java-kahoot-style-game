e55097b88e4bf2699be0dd673a77bb7911c6674f
tan05 <tan.tantham.tan@gmail.com>
2023-12-27 21:07:05 +0700

    added comments

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     //messageType: 0 == x, 1 == info
 
-    static String[] quizzes = {"Default"};
+    static String[][] quizzes = {{"default", "default quiz", "what is 1 + 1"}}; //create the quiz array
     static String windowTitle = "Trash kahoot";
 
-    public static int quit() {
+    public static int quit() { //method for quitting
         int option = Integer.parseInt(JOptionPane.showInputDialog("Are you sure you want to quit \n(0) y\n(1) n"));
 
         if (option == 0){
-            System.exit(0);
+            System.exit(0); //check if user wants to quit
         }
 
         return 0;
@@ -19,24 +19,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JOptionPane.showMessageDialog(null, "Welcome to my bad kahoot clone", windowTitle, 1);
+        JOptionPane.showMessageDialog(null, "Welcome to my bad kahoot clone", windowTitle, 1); //welcome message
             
-        boolean inMainmenue = true;
+        boolean inMainmenue = true; //createa variable to check if in main menue
 
             while (inMainmenue) {
 
                 int usrOption = Integer.parseInt(JOptionPane.showInputDialog("(1) create quiz\n(2) play quiz\n(3) quit app")); //get usr input
 
-                System.out.println(usrOption);
+                System.out.println(usrOption); //prints out usr option
 
-                if (usrOption == 1){
+                if (usrOption == 1){ //code if usr chooses to create a quiz
                     System.out.println("Create quiz");
                     MakeQuiz.makeQuiz();
                 }
-                else if (usrOption == 2){
+                else if (usrOption == 2){ //code when usr chooses to play a quiz
                     System.out.println("Played quiz");
                 }
-                else if (usrOption == 3){
+                else if (usrOption == 3){ //check when ur quits
                     System.out.println("Quit");
                     quit();
                 }
--- MakeQuiz.java
+++ MakeQuiz.java
@@ -2,10 +2,11 @@ import javax.swing.JOptionPane;
 
 public class MakeQuiz {
     public static String[] makeQuiz() {
-        String[] newQuiz = {};
 
         String quizName = JOptionPane.showInputDialog("Input quiz name");
         String quizDescription = JOptionPane.showInputDialog("Input quiz description");
+
+        String[] newQuiz = {quizName, quizDescription};
 
         return newQuiz;
     }
